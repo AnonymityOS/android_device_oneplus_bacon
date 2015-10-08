c353608e33d06c3aad0dde81f5db112faf214be8
Hamster Tian <haotia@gmail.com>
2015-10-08 20:31:31 +0800

    aosp_bacon: full_base_telephony -> aosp_base_telephony + APN list
    * Seems CM moved APN list inclusion to vendor
    * Where is my Messaging app?

--- aosp_bacon.mk
+++ aosp_bacon.mk
@@ -12,8 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Get the long list of APNs
+PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
+
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit from bacon device
 $(call inherit-product, device/oneplus/bacon/bacon.mk)
